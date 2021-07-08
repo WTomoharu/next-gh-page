3adc7b38f2b6f9a4f4e7dd5cc30e98d5bdb37a18
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-07-08 18:06:44 +0900

    next-configを変更

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  basePath: process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '',
   assetPrefix: process.env.URL_PREFIX ? '/' + process.env.URL_PREFIX : '',
   trailingSlash: true,
   reactStrictMode: true,
