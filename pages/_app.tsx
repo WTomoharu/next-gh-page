481d0837994f5d675fdf689b81d61f03f0316646
Tomoharu Watanabe <watatomo1216@gmail.com>
2021-07-08 18:39:12 +0900

    Navを修正

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,26 +5,40 @@ import Link from 'next/link';
 
 import '../styles/globals.css';
 
-const Nav = () => (
-  <nav>
-    <Link href="/">Index</Link>
-    {' | '}
-    <Link href="/about">About</Link>
-  </nav>
-)
+const Nav = () => {
+  const links: { text: string, path: string }[] = [
+    { text: "Index", path: "/" },
+    { text: "About", path: "/about" },
+  ]
+
+  return (
+    <nav>
+      <ul>
+        {links.map((link, i) => (
+          <li key={i} style={{ display: "inline" }}>
+            {0 < i ? " | " : ""}
+            <Link href={link.path}>
+              <a className="underline">{link.text}</a>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  )
+}
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
-    <Head>
-      <title>Next-GH-Pages</title>
-    </Head>
+      <Head>
+        <title>Next-GH-Pages</title>
+      </Head>
 
-    <>
-      <Nav />
-      <Component {...pageProps} />
+      <>
+        <Nav />
+        <Component {...pageProps} />
+      </>
     </>
-  </>
   )
 }
 
